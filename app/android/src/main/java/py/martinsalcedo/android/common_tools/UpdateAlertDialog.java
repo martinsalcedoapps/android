a41caabdbab3fb4a6334851771455e16fee0c1cc
Martin <martinsalcedo.apps@gmail.com>
2021-01-24 00:34:22 -0300

    [FIX] Translates

--- app/android/src/main/java/py/martinsalcedo/android/common_tools/UpdateAlertDialog.java
+++ app/android/src/main/java/py/martinsalcedo/android/common_tools/UpdateAlertDialog.java
@@ -10,11 +10,11 @@ import py.martinsalcedo.android.R;
 
 public class UpdateAlertDialog {
 
-    public static Dialog updateConfirmation(Activity activity, final View view, String old_version, String new_version) {
+    public static Dialog updateConfirmation(Activity activity, final View view, String curVersion, String thisVersion) {
         AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
 
         dialog.setTitle(activity.getString(R.string.Confirmation));
-        dialog.setMessage(String.format("%s/%s - %s", old_version, new_version, activity.getString(R.string.ThereIsNewUpdate)));
+        dialog.setMessage(String.format("%s:%s/%s:%s - %s", activity.getString(R.string.YourVersion),thisVersion, activity.getString(R.string.NewVersion),curVersion, activity.getString(R.string.ThereIsNewUpdate)));
         dialog.setPositiveButton(activity.getString(R.string.UpdateNow), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
