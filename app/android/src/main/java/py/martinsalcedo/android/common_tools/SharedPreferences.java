76ae4304e816373eed9fcf9689bc7f4bf7c142e6
Martin <martinsalcedo.apps@gmail.com>
2021-01-24 20:01:28 -0300

    [NEW] StringSet preferences

--- app/android/src/main/java/py/martinsalcedo/android/common_tools/SharedPreferences.java
+++ app/android/src/main/java/py/martinsalcedo/android/common_tools/SharedPreferences.java
@@ -2,6 +2,10 @@ package py.martinsalcedo.android.common_tools;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class SharedPreferences {
 
     //###############	SHARED PREFERENCES ##########
@@ -33,4 +37,24 @@ public class SharedPreferences {
         android.content.SharedPreferences sharedPref = context.getSharedPreferences(appPackageName, Context.MODE_PRIVATE);
         return sharedPref.getString(key, "");
     }
+
+    public static void setStringSetPreference(Context context, String key, ArrayList elements) {
+        final String appPackageName = context.getPackageName();
+        android.content.SharedPreferences sharedPref = context.getSharedPreferences(appPackageName, Context.MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = sharedPref.edit();
+        Set<String> stringSet = new HashSet<>();
+//        editor.putInt(key, value);
+        for (int idx = 0; idx < elements.size(); idx++) {
+            stringSet.add(String.format("%s|%s", idx, elements.get(idx)));
+        }
+        editor.putStringSet(key, stringSet);
+        editor.apply();
+    }
+
+    public static Set<String> getStringSetPreference(Context context, String key) {
+        final String appPackageName = context.getPackageName();
+        android.content.SharedPreferences sharedPref = context.getSharedPreferences(appPackageName, Context.MODE_PRIVATE);
+        return sharedPref.getStringSet(key, new HashSet<String>(0));
+    }
+
 }
